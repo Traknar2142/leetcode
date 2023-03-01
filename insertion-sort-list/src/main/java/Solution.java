22639abbc13dd08139ac618fc7edf6d9bea55b0c
vprokhorenko <traknar2142@gmail.com>
2023-03-01 19:02:17 +0400

    -

--- insertion-sort-list/src/main/java/Solution.java
+++ insertion-sort-list/src/main/java/Solution.java
@@ -1,5 +1,28 @@
 class Solution {
     public ListNode insertionSortList(ListNode head) {
-        return null;
+        ListNode sortedListNode = new ListNode();
+
+        //////////////////////////////////////////////////
+        ListNode prev = sortedListNode; //ссылка на отсортированный лист
+        ListNode curr = head; //указатель на конкретную ноду
+        ListNode next; // указатель на следующую ноду
+        ///////////////////////////////////////////////////
+
+        while (curr != null) {
+            next = curr.next;
+
+            while (prev.next != null && prev.next.val < curr.val) {//[3,5,6,1,4,2]
+                prev = prev.next;
+            }
+
+            curr.next = prev.next;
+            prev.next = curr;
+
+            prev = sortedListNode;
+
+            curr = next;
+        }
+
+        return sortedListNode.next;
     }
 }
--- insertion-sort-list/src/test/java/SolutionTest.java
+++ insertion-sort-list/src/test/java/SolutionTest.java
@@ -1,14 +1,19 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SolutionTest {
 
     @Test
     void test() {
-        ListNode listNode = buildListNode(new int[]{1, 2, 3, 4});
-        System.out.println(listNode);
+        ListNode listNode = convertToListNode(new int[]{3,5,6,1,4,2});
+        ListNode actual = new Solution().insertionSortList(listNode);
+        int[] expected = new int[]{1,2,3,4,5,6};
+        int[] actualArray = convertToIntArray(actual);
+
+        Assertions.assertEquals(expected,actualArray);
     }
 
-    private ListNode buildListNode(int[] arr) {
+    private ListNode convertToListNode(int[] arr) {
         ListNode head = new ListNode(arr[0]);
         ListNode currentNode = head;
         ListNode nextNode;
@@ -20,5 +25,29 @@ public class SolutionTest {
             }
         }
         return head;
+    }
+
+    private int[] convertToIntArray(ListNode listNode) {
+        int countOfNodes = getContOfNodes(listNode);
+        int[] arr = new int[countOfNodes];
+        ListNode currentNode = listNode;
+        ListNode nextNode;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = currentNode.val;
+            currentNode = currentNode.next;
+        }
+        return arr;
+    }
+
+    private int getContOfNodes(ListNode listNode) {
+        int count = 1;
+        while (listNode.next != null) {
+            ListNode pointer = listNode.next;
+            if (pointer != null) {
+                count++;
+                listNode = pointer;
+            }
+        }
+        return count;
     }
 }
