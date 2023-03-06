701d5c4b8dab80ae6e92f0d599b89d4aeeb7a728
vprokhorenko <traknar2142@gmail.com>
2023-03-07 00:22:03 +0400

    Heap sort fix

--- heapify-example/src/main/java/Solution.java
+++ heapify-example/src/main/java/Solution.java
@@ -1,48 +1,40 @@
 public class Solution {
     public static void main(String[] args) {
-        int[] arr = new int[]{7, 3, 2, 5, 6, 10, 9, 8, 1};
+        int[] arr = new int[]{5,2,3,1};
 
         for (int i = arr.length / 2 - 1; i >= 0; i--) {
             heapify(arr, arr.length, i);
         }
 
-        printArr(arr); //[10,8,9,5,6,2,7,3,1]
-
         for (int i = arr.length - 1; i > 0; i--) {
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
+            int temp = arr[i];
+            arr[i] = arr[0];
+            arr[0] = temp;
             heapify(arr, i, 0);
         }
-        int temp = arr[0];
-        arr[0] = arr[1];
-        arr[1] = temp;
-
         printArr(arr);
-
-
     }
 
-    public static int[] heapify(int[] arr, int heapSize, int index) {
-        while (index <= heapSize / 2 - 1) {
-            int leftNode = index * 2 + 1;
-            int rightNode = index * 2 + 2;
-            if ((arr[index] < arr[leftNode] || arr[index] < arr[rightNode]) && rightNode < heapSize) {
-                if (arr[rightNode] < arr[leftNode]) {
-                    int temp = arr[leftNode];
-                    arr[leftNode] = arr[index];
-                    arr[index] = temp;
-                    index = leftNode;
-                } else {
-                    int temp = arr[rightNode];
-                    arr[rightNode] = arr[index];
-                    arr[index] = temp;
-                    index = rightNode;
-                }
-            } else break;
+    public static void heapify(int[] arr, int heapSize, int index) {
+        int leftNode = index * 2 + 1;
+        int rightNode = index * 2 + 2;
+        int largest = index;
+
+        if (leftNode < heapSize && arr[leftNode] > arr[largest]) {
+            largest = leftNode;
         }
 
-        return arr;
+        if (rightNode < heapSize && arr[rightNode] > arr[largest]) {
+            largest = rightNode;
+        }
+
+
+        if (index != largest) {
+            int temp = arr[index];
+            arr[index] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, heapSize, largest);
+        }
     }
 
     public static void printArr(int[] arr) {
